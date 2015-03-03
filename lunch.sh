0685d585953d3d5d7329a97945d8aebcf8fde303
lgrn <linus@agren.cc>
2015-03-03 15:53:32 +0100

    Shorter lines
    Might improve readability, or it might not.

--- lunch.sh
+++ lunch.sh
@@ -3,7 +3,12 @@ MAILTO="alice@example.com bob@example.org"
 MAILFROM="lunch_script@example.edu"
 if hash curl 2>/dev/null; then
   if hash lynx 2>/dev/null; then
-    curl -s barabicu.se | awk -F '<li>' '{printf $2}' | awk -F '</li>' '{printf $2}' > barabicu.html && lynx -assume_charset=utf-8 -display_charset=utf-8 -dump barabicu.html | mailx -r $MAILFROM -s "Dagens Lunch - "$(date +"%A %d/%m-%Y") $MAILTO  
+    curl -s barabicu.se | \
+    awk -F '<li>' '{printf $2}' | \
+    awk -F '</li>' '{printf $2}' > \
+    barabicu.html && lynx -assume_charset=utf-8 -display_charset=utf-8 -dump barabicu.html | \
+    mailx -r $MAILFROM -s "Dagens Lunch - "$(date +"%A %d/%m-%Y") $MAILTO  
+    
     rm barabicu.html
   else
     echo "Error: Lynx is not available, exiting!"
