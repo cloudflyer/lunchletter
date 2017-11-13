d60c0732f6788472ef48fbe145cebbac573e4c88
cloudflyer <cloudflyer@users.noreply.github.com>
2017-11-13 13:13:21 +0100

    Update lunch_indiskahornet.sh
    Modifications to work with the new webpage.

--- lunch_indiskahornet.sh
+++ lunch_indiskahornet.sh
@@ -4,16 +4,23 @@ MAILTO="bob@example.com scott@example.com"
 MAILFROM="noreply@example.com"
 TODAY=`date +%A`
 TODAYNUM="$`date --date='+1 day' +%w`"
-
-export LANG=sv_SE.UTF-8
 if hash curl 2>/dev/null; then
   if hash lynx 2>/dev/null; then
-    curl -s http://indiskahornet.kvartersmenyn.se | sed -e '1,/class=\"day\"/d' \
-        -e '/End\ Menu\ List/,$d' | awk -F '<strong>' '{printf '$TODAYNUM'}' >> indiskahornet.html && \
-    lynx -assume_charset=utf-8 -display_charset=utf-8 -dump indiskahornet.html | \
-    mailx -r $MAILFROM -s "Dagens Lunch - Indiska Hörnet - "${TODAY^} $MAILTO
+    if [ "$TODAY" != "fredag" ]; then
+      curl -s http://www.indiskahornet.se/lunch_meny.html | sed -e '1,/'$TODAY'/Id' -e '/<h3>/,$d' >>\
+      indiskahornet.html && echo "<br/><br/>Source: http://www.indiskahornet.se" >> \
+      indiskahornet.html && lynx -cfg <(echo COLLAPSE_BR_TAGS:FALSE) -assume_charset=utf-8 -display_charset=utf-8 -nolist -dump indiskahornet.html | \
+      mailx -r $MAILFROM -s "Dagens Lunch - Indiska Hörnet - "${TODAY^} $MAILTO
 
-    rm indiskahornet.html
+      rm indiskahornet.html
+    else
+      curl -s http://www.indiskahornet.se/lunch_meny2.html | sed -e '1,/'$TODAY'/Id' -e '/<h3>/,$d' >>\
+      indiskahornet.html && echo "<br/><br/>Source: http://www.indiskahornet.se" >> \
+      indiskahornet.html && lynx -cfg <(echo COLLAPSE_BR_TAGS:FALSE) -assume_charset=utf-8 -display_charset=utf-8 -nolist -dump indiskahornet.html | \
+      mailx -r $MAILFROM -s "Dagens Lunch - Indiska Hörnet - "${TODAY^} $MAILTO
+
+      rm indiskahornet.html
+    fi 
   else
     echo "Error: Lynx is not available, exiting!"
     exit 1
